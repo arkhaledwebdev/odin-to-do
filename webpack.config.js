93f9b3d36f42e3741c3c1fa4d547c40c306327fc
arkhaledwebdev <arkhaledwebdev@gmail.com>
2023-12-07 15:34:33 +0200

    Minor design modifications

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Odin To-Do',
